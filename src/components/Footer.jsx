bbba689ea3874765343297c7e43879822826bb90
LucasAlbarado <lucas.miguel.albarado24@gmail.com>
2023-09-27 14:20:38 -0300

    fixed-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Container, Row, Col, Navbar, Nav, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Footer.css'
+import logoFooter from './images/logo-footer.jpg';
+import { Container, Row, Col, Navbar, Nav, Button } from 'react-bootstrap';
 import { FaFacebook, FaInstagram, FaTwitter, FaPhoneAlt, FaRegEnvelope, FaRegPaperPlane } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-
-
-
 
 const Footer = () => {
   return (
@@ -15,7 +13,7 @@ const Footer = () => {
               <div className="container">
                   <div className="row">
                       <div className="col-md-4 col-lg-4 col-12 ft-1 logo-icons">
-                      <Link to="/"><img className='logo-footer ' src="./src/images/logo-footer.jpg" alt="logo de la pagina" /></Link>
+                      <Link to="/"><img className='logo-footer ' src="{logoFooter}" alt="logo de la pagina" /></Link>
                           <div className="footer-icons pt-3 ">
                           <Link to="/Error404" className='m-2'><FaFacebook  className='link-nav'/></Link>
                           <Link to="/Error404" className='m-2'><FaInstagram  className='link-nav'/></Link>
