286f66296659c9a4c842f8fde5aedde19668bd48
JoaqAb <joaquinabuin@gmail.com>
2023-09-27 10:49:17 -0300

    fix Footer.css import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Navbar, Nav, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './footer.css'
+import './Footer.css'
 import { FaFacebook, FaInstagram, FaTwitter, FaPhoneAlt, FaRegEnvelope, FaRegPaperPlane } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
