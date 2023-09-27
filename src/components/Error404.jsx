715140f8afaad7fc7fe7ac2703e973ee64d2d964
JoaqAb <joaquinabuin@gmail.com>
2023-09-27 10:40:01 -0300

    fix a tag

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import  './error404.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./error404.css";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <div className='container-404'>
-       
-        <img src="/src/images/error-404.png" className='img-404'alt="imagen 404"/>
-        <h1 className='text-center'>Lo sentimos mucho, la página que ha solicitado no se encuentra disponible :c</h1>
+    <div className="container-404">
+      <img
+        src="/src/images/error-404.png"
+        className="img-404"
+        alt="imagen 404"
+      />
+      <h1 className="text-center">
+        Lo sentimos mucho, la página que ha solicitado no se encuentra
+        disponible :c
+      </h1>
+      <a href="/">
         <div className="boton">
-        <Link to="/"><button>Volver al inicio</button></Link>
+          <Link to="/">
+            <button>Volver al inicio</button>
+          </Link>
         </div>
       </a>
     </div>
