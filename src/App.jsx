9a277023df9a28325fe1c932eeb15476f004f7c4
LucasAlbarado <lucas.miguel.albarado24@gmail.com>
2023-09-27 00:06:17 -0300

    ordering

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
   import React from 'react'
   import { Formik } from 'formik';
   import 'bootstrap/dist/css/bootstrap.min.css';
-  import { BrowserRouter, Route, Routes } from 'react-router-dom';
   import 'semantic-ui-css/semantic.min.css'
+  import { BrowserRouter, Route, Routes } from 'react-router-dom';
   import NavBar from './components/Navbar';
   import Footer from './components/Footer';
-
   import Aboutus from './components/Aboutus';
+  import App from "../components/App";
   import Formulario  from './components/Contact';
-  import Frequentquestion from './components/Frequentquestion';
+  import CuentaScreen from "../components/CuentaScreen";
   import Error404 from './components/Error404';
+  import Frequentquestion from './components/Frequentquestion';
+  import HomeScreen from "../components/HomeScreen";
+  import { Login } from "../components/Login";
+  import { RegisterScreen } from "../components/RegisterScreen";
+
 
   function App() {
     return (  
@@ -18,10 +23,15 @@
         <BrowserRouter>
           <NavBar/>
             <Routes>
-                <Route path= "/Contact" element = {<Formulario/>}/> 
                 <Route path= "/Aboutus" element = {<Aboutus/>}/> 
-                <Route path= "/Frequentquestion" element = {<Frequentquestion/>}/> 
+                <Route path="/App" element={<App />} />
+                <Route path= "/Contact" element = {<Formulario/>}/> 
+                <Route path="/cuenta" element={<CuentaScreen/> }/>
                 <Route path= "/Error404" element = {<Error404/>}/> 
+                <Route path= "/Frequentquestion" element = {<Frequentquestion/>}/> 
+                <Route path="/" element={<HomeScreen />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<RegisterScreen />} />
             </Routes>
           <Footer/>
         </BrowserRouter>
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-
 import CuentaScreen from "./CuentaScreen";
+
 
 function App() {
   const [usuario, setUsuario] = useState({
