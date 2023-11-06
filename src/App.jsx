de8bd156c9f6167a0dad9166c76e04d9b256fe72
LucasAlbarado <lucas.miguel.albarado24@gmail.com>
2023-11-06 11:20:52 -0300

    routing and functionalities ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,59 @@
   import { BrowserRouter, Route, Routes } from 'react-router-dom';
   import NavBar from './components/Navbar';
   import Footer from './components/Footer';
+  import NavbarComponent from './components/Navbar';
+
   import AboutUs from './components/AboutUs';
   import AppAuth from "./components/App";
   import Formulario  from './components/Contact';
+  import MenuPage from './components/MenuPage';
+  import CartPage from './components/CartPage';
+
   import CuentaScreen from "./components/CuentaScreen";
   import Error404 from './components/Error404';
   import Frequentquestion from './components/Frequentquestion';
   import HomeScreen from "./components/HomeScreen";
   import { Login } from "./components/Login";
   import { RegisterScreen } from "./components/RegisterScreen";
+  import { useAppFuncionalidades } from './components/Funcionalidades';
+
 
 
   function App() {
+    const { numeroMesa, mostrarMesaInput, cart, estadoPedido, handleNuevoPedido, handleNumeroMesaSubmit, addToCart } = useAppFuncionalidades();
     return (  
       
       <>
         <BrowserRouter>
-          <NavBar/>
+        <NavbarComponent
+        numeroMesa={numeroMesa}
+        cartCount={cart.length}
+        estadoPedido={estadoPedido}
+        handleNuevoPedido={handleNuevoPedido}
+      />
             <Routes>
                 <Route path= "/Aboutus" element = {<AboutUs/>}/> 
                 <Route path="/App" element={<AppAuth />} />
+                <Route path="/cart" element={<CartPage />} />
                 <Route path= "/Contact" element = {<Formulario/>}/> 
                 <Route path="/account" element={<CuentaScreen/> }/>
                 <Route path= "/Error404" element = {<Error404/>}/> 
                 <Route path= "/Frequentquestion" element = {<Frequentquestion/>}/> 
                 <Route path="/" element={<HomeScreen />} />
+                <Route path="/menu" element={<MenuPage addToCart={addToCart} />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<RegisterScreen />} />
-            </Routes>
-          <Footer/>
+                {mostrarMesaInput && (
+        <MesaModal
+          show={mostrarMesaInput}
+          onClose={() => handleNuevoPedido()}
+          onSubmit={handleNumeroMesaSubmit}
+        />
+      )}
+              </Routes>
+            <Footer/>
         </BrowserRouter>
-      </>
+        </>
     )
   }
 
